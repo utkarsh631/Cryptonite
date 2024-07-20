39722149f42a905069a650d2b8f256050010055e
utkarsh631 <utkarshji2023@gmail.com>
2024-07-20 19:53:07 +0530

    Update Drawer.js

--- src/components/common/Drawer.js
+++ src/components/common/Drawer.js
@@ -3,7 +3,7 @@ import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
 import Switch from '@mui/material/Switch';
-
+import { Link } from 'react-router-dom';
 export default function AnchorTemporaryDrawer() {
   const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
@@ -19,10 +19,18 @@ export default function AnchorTemporaryDrawer() {
           >
            <div className='  flex flex-col p-2 gap-4 items-start justify-start bg-slate-800 h-full mx-auto '>
            <div className=''><Switch {...label} defaultChecked /></div>
+          <Link to={'/'}>
           <div  className='  text-gray-300 hover:text-white hover:scale-105 cursor-pointer transition-all duration-100 '>Home</div>
+          </Link>
+          <Link to={'/compare'}>
           <div className=' text-gray-300 hover:text-white hover:scale-105 cursor-pointer transition-all duration-100'>Compare</div>
+          </Link>
+          <Link to={'/watchlist'}>
           <div className=' text-gray-300 hover:text-white hover:scale-105 cursor-pointer transition-all duration-100'>Watchlist</div>
+          </Link>
+          <Link to={'/dashboard'}>
           <div className=' rounded-full p-1  text-gray-300 hover:text-white hover:scale-105 transition-all duration-100 cursor-pointer'>Dashboard</div>
+          </Link>
            </div>
           </Drawer>
     </div>
